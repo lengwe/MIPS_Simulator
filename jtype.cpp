c893e545f82f9d728031b3a4e152678068ee1b1a
lengwe <wl4817@ic.ac.uk>
2018-11-22 19:28:46 +0000

    jtype_changed

--- jtype.cpp
+++ jtype.cpp
@@ -10,7 +10,7 @@ void j_type(uint32_t r[32], uint32_t ins,uint32_t *ins_mem,
             uint8_t *data_mem,uint64_t &hilo,unsigned int &hi,
             unsigned int &lo,uint32_t &pc, uint32_t &count){
 
-  jtype decode;uint64_t
+  jtype decode;
   decode.opcode = ins>>26;
   decode.target = ins<<6>>6;
 
